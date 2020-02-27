3e55669575c58298ef971c3472cc6daed348486e
Justin <Justin@ClassID372>
2020-02-27 14:10:37 -0700

    First Version: Updated methods add, and remove
    Add method extends array length by 50% when the array is full, instead
    of just letting the last value fall off
    
    Remove method reduces the array length by 25% when the array is more
    than 25% empty.

--- cse360assign2/src/cse360assign2/SimpleList.java
+++ cse360assign2/src/cse360assign2/SimpleList.java
@@ -9,78 +9,119 @@ package cse360assign2;
  */
 public class SimpleList 
 {
-	/**
-	 * MAX_ARRAY_ELEMENTS is the maximum elements the integer array list can hold
-	 * list is the integer array in which the elements for the class will be stored
-	 * count stores the number of elements currently in the int array list
-	 */
-    final int MAX_ARRAY_ELEMENTS = 10;
+    /**
+     * maxArrayElements is the maximum elements the integer array list can hold
+     * list is the integer array in which the elements for the class will be stored
+     * count stores the number of elements currently in the int array list
+     */
+    private int maxArrayElements = 10;
     private int[] list;
     private int count;
-
-
+    
+    
     /**
      * Sole constructor, sets count to 0 and creates an integer array of size
-     * MAX_ARRAY_ELEMENTS currently set to 10
+     * maxArrayElements currently set to 10
      */
     public SimpleList()
     {
         count = 0;
-        list = new int[MAX_ARRAY_ELEMENTS];
+        list = new int[maxArrayElements];
     }
-
-
+    
+    
     /**
      * Method for adding an integer into the array at index 0
      * This method shifts all other numbers in the array back by one
+     * If the array is full a copy with increased size of 50%  will be made
+     * The values will be copied and old array will be replaced with the new
      * Increases the count by 1.
      * 
      * @param numberAdding The number you want to add to the array
      */
     public void add(int numberAdding)
     {
-        if(count < MAX_ARRAY_ELEMENTS)
+        int newMaxElements;
+        
+        if(count == maxArrayElements)
         {
-        	// Moving each element back one spot if array is less than 10 elements
-            for(int iterator = count; iterator > 0; iterator--)
-                list[iterator] = list[iterator - 1];
-            count++;
+            // Calculate new Max by 50%
+            newMaxElements = maxArrayElements + maxArrayElements / 2;
+            
+            // Make new array with bigger size
+            int[] copyList = new int[newMaxElements];
+            
+            // Copy all values over from list
+            for(int iterator = 0; iterator < count; iterator++)
+                copyList[iterator] = list[iterator];
+            
+            // Have list point to the new list made with 50% more elements
+            list = copyList;
+            
+            // Setting new max
+            maxArrayElements = newMaxElements;
         }
-        else
-        	// Moving elements back and having the last one fall off if array is full
-            for(int iterator = count - 1; iterator > 0; iterator--)
-                list[iterator] = list[iterator - 1];
+        
+        // Moving each element back one spot
+        for (int iterator = count; iterator > 0; iterator--)
+            list[iterator] = list[iterator - 1];
 
-           list[0] = numberAdding;
+
+        count++;
+        list[0] = numberAdding;
     }
-
-
+    
+    
     /**
      * Method of removing an integer from the array. The number provided will remove
      * all instances of the element in the array. Array will then be shifted and count
      * will be reduced by the number of elements removed.
+     * If the array is more than 25% empty array is reduced in size by 25%
      * 
      * @param numberRemoving The integer you want to be removed for the array
      */
     public void remove(int numberRemoving)
     {
+        int newMaxElements;
+        
         for(int iterator = 0; iterator < count; iterator++)
         {
             if(list[iterator] == numberRemoving)
             {
                 for(int j = iterator; j < count - 1; j++)
                     list[j] = list[j + 1];
-
+                
                 count--;
-
+                
                 // If element is found and deleted, iterator is moved back one to
                 // check if element is found in same index after shift
                 iterator--;
             }
         }
+        
+        // Checks if the array is 25% empty
+        if( (maxArrayElements - count) > (maxArrayElements / 4) )
+        {
+            // Calculate new Max by reducing by 25%
+            newMaxElements = maxArrayElements - maxArrayElements / 4;
+            
+            // Make new array with bigger size
+            int[] copyList = new int[newMaxElements];
+            
+            // Copy all values over from list
+            for(int iterator = 0; iterator < count; iterator++)
+                copyList[iterator] = list[iterator];
+            
+            // Have list point to the new list made with 25% less elements
+            list = copyList;
+            
+            // Setting new max
+            maxArrayElements = newMaxElements;
+        }
+        
     }
-
-
+    
+    
     /**
      * Returns the number of elements currently in the array.
      * 
@@ -90,8 +131,8 @@ public class SimpleList
     {
         return count;
     }
-
-
+    
+    
     /**
      * Returns every element currently in the array separated by spaces.
      * 
@@ -100,19 +141,19 @@ public class SimpleList
     public String toString()
     {
         String listOfAllElements = "";
-
+        
         // Adds a space after each element loop does not add last element to string
         for(int iterator = 0; iterator < count - 1; iterator++)
             listOfAllElements = listOfAllElements + list[iterator] + " ";
-
+        
         //Adds last element, if only one element makes it the return value
         if(count != 0)
-        	listOfAllElements = listOfAllElements + list[count - 1];
-
+            listOfAllElements = listOfAllElements + list[count - 1];
+        
         return listOfAllElements;
     }
 
-
+    
     /**
      * Method used to find an integers first location in the array.
      * 
@@ -123,9 +164,9 @@ public class SimpleList
      */
     public int search(int numberToFind)
     {
-    	int indexLocation = -1;
-    	boolean numNotFound = true;
-
+        int indexLocation = -1;
+        boolean numNotFound = true;
+        
         for(int iterator = 0; iterator < count; iterator++)
             if(numNotFound  && list[iterator] == numberToFind)
             {
@@ -135,27 +176,27 @@ public class SimpleList
         return indexLocation;
     }
 
-
+    
     /**
      * Returns the full integer array
      * 
      * @return Returns the integer array
      */
-	public int[] getList()
-	{
-		return list;
-	}
-
-
-	/**
-	 * Method used to get the value of a specific index in the array
-	 * 
-	 * @param index An integer at the index in which you want the value
-	 * @return Returns the value in the array at the provided index
-	 */
-	public int getValueAtIndex(int index)
-	{
-		return list[index];
-	}
+    public int[] getList()
+    {
+        return list;
+    }
+    
+    
+    /**
+     * Method used to get the value of a specific index in the array
+     * 
+     * @param index An integer at the index in which you want the value
+     * @return Returns the value in the array at the provided index
+     */
+    public int getValueAtIndex(int index)
+    {
+        return list[index];
+    }
 
 }
